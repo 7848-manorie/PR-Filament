d5c58ac436636001ba47d2e8a1014aee7efed651
7848-manorie <166717416+7848-manorie@users.noreply.github.com>
2024-12-24 18:33:26 +0530

    Update prog4-pass.c

--- prog4-pass.c
+++ prog4-pass.c
@@ -12,15 +12,12 @@ void main(){
         int lower=0,digit=0,special=0;
         p=strlen(pass);
         for(int i=0;i<p;i++){
-            // if((pass[i]>='a' && pass[i]<='z') || (pass[i]>='0' && pass[i]<='9') ||
-            //     pass[i]=='!' || pass[i]=='@' ||pass[i]=='#' || pass[i]=='.')
             if(pass[i]>='a' && pass[i]<='z')
                 ++lower;
             if(pass[i]>='0' && pass[i]<='9')
                 ++digit;
             if(pass[i]=='!' || pass[i]=='@' ||pass[i]=='#' || pass[i]=='.')
                 ++special;
-                // printf("Your password is strong");
         }
         if(lower>0 && digit>0 && special>0){
             printf("Your password is strong");
